45b21b58e7197222fb1200b0f1d9fe40666e5b0c
Ehtesham Zulfiqar <ehtesham.zulfiqar@gmail.com>
2022-11-17 14:57:58 -0500

    fix(server): fixed git deps and errors

--- server/server.js
+++ server/server.js
@@ -1,20 +1,19 @@
-const fastify = require("fastify");
-const cors = require("fastify-cors");
+const cors = require("@fastify/cors");
+const fastify = require("fastify")({
+  logger: true,
+});
 
 const PORT = 8000;
-const server = fastify({ logger: true });
 
-server.register(cors, { origin: true });
+fastify.register(cors, { origin: true });
 
-// Add API endpoints here
-
-async function start() {
+const start = async () => {
   try {
-    await server.listen(PORT);
+    await fastify.listen({ port: PORT });
   } catch (err) {
-    console.error(err);
+    fastify.log.error(err);
     process.exit(1);
   }
-}
+};
 
 start();
--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,13 @@
+import { Container } from "@mui/material";
 import React from "react";
 import { Books } from "./modules/Books";
 
 function App() {
   return (
     <div className="App">
-      <Books />
+      <Container maxWidth="md">
+        <Books />
+      </Container>
     </div>
   );
 }
